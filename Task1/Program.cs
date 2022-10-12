c46ca164eaaf25d4c71c3265ffa85a3efa3c3617
Kirill Grigorev <grigorevkirill@mail.ru>
2022-10-12 23:14:54 +0400

    added ReadData and PrintData methods

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//=====================================================================================
+// Написать прграмму, которая из имеющегося массива строк формирует новый массив из 
+// строк, длина которых меньше или равна 3 символам.
+//=====================================================================================
+
+// Считываем данные и парсим строку в массив строк
+string[] ReadData(string line)
+{
+    Console.Write(line);
+    string inputLine = Console.ReadLine() ?? " ";
+    string[] stringArray = inputLine.Split(',', StringSplitOptions.TrimEntries);
+    return stringArray;
+}
+// Выводим результат
+void PrintData(string[] stringArray)
+{
+    Console.Write($"[<{stringArray[0]}>, ");
+    for(int i =1; i < stringArray.Length - 1; i++)
+    {
+        Console.Write($"<{stringArray[i]}>, ");
+    }
+    Console.WriteLine($"<{stringArray[stringArray.Length - 1]}>]");
+}
+
+string[] stringArray = ReadData("Введите строки через запятую: ");
+PrintData(stringArray);
